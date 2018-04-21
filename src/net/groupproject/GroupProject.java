eddf3983665e6e9127f605ec8141695ed460229f
Jacky2611 <jacky2611@gmail.com>
2018-04-21 16:13:56 +0200

    first package and class, intellij settings setup

---
+++ src/net/groupproject/GroupProject.java
@@ -0,0 +1,16 @@
+package net.groupproject;
+
+/**
+ * @author Ansraer on 21.04.2018
+ * @author Jakob Lang
+ * @project TeamaufgabeProjekt
+ */
+public class GroupProject {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
